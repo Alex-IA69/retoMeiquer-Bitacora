0b5bb60665761f7462ad15616755af3112d11d9c
Alex-IA69 <A01635557@tec.mx>
2023-06-15 16:23:13 -0600

    Modificaciones server

--- server/server.js
+++ server/server.js
@@ -1,134 +1,123 @@
-// server.js
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const cors = require('cors');
-
-// Crear la aplicación Express
 const app = express();
+const bodyParser = require('body-parser');
 
-// Permitir solicitudes CORS desde cualquier origen
+app.use(bodyParser.json());
 app.use(cors());
-
-// Parsear el body de las solicitudes POST
 app.use(express.json());
 
-// Crear conexión a la base de datos
+// CONEXION BASE DE DATOS
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'benja',
-  password: '12345',
+  user: 'testuser',
+  password: 'passw0rdnT.!',
   database: 'bitacora'
 });
 
-// Conectar a la base de datos
 db.connect((err) => {
   if (err) throw err;
   console.log('Conectado a la base de datos');
 });
 
-// Definir la ruta para obtener los asistentes
-app.get('/api/asistentes', (req, res) => {
-  const sql = 'SELECT * FROM asistentes';
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(result);
+// DEFINICION DE APIS
+// -------- GENERAL
+// get 
+app.get('/:table/:field?', (req, res) => {
+  const {table, field} = req.params;
+
+  let query = `SELECT ${field || '*'} FROM ${table}`;
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error ejecutando siguiente query: ', err);
+      // Query mal redactado / tabla no existe
+      res.status(500).json({ error: 'Error ejecutando query' }); 
+      return;
+    }
+
+    if (results.length === 0) {
+      // Tabla existe, no tiene datos
+      res.status(200).json({ message: 'No hay datos en la tabla o campo' }); 
+      return;
+    }
+
+    if (results.length > 0) { 
+      // Tabla existe, tiene datos
+      res.json(results);
+      return;
+    }
+
+    res.status(404).json({ error: 'No se encontro la tabla o campo' });
   });
 });
 
-// Definir la ruta para crear un asistente
-app.post('/api/asistentes', (req, res) => {
-  const { nombre, tipo } = req.body;
-  const sql = 'INSERT INTO asistentes SET ?';
-  const asistente = { nombre, tipo };
-  db.query(sql, asistente, (err, result) => {
-    if (err) throw err;
-    res.send(result);
+// -------- PARTICIPANTES
+// post
+app.post('/participantes', (req, res) => {
+  const participantes = req.body;
+
+  const query = `INSERT IGNORE INTO participantes (nombre, tipo) VALUES ?`;
+  const values = participantes.map(participante => [participante.nombre, participante.tipo]);
+
+  db.query(query, [values], (err, result) => {
+    if (err) {
+      console.error('Error al guardar los participantes:', err);
+      res.status(500).send('Error al guardar los participantes');
+      return;
+    }
+
+    const insertedCount = result.affectedRows;
+    if (insertedCount === 0) {
+      res.status(200).json({ message: 'No se insertaron nuevos participantes' });
+    } else {
+      res.status(201).json({ message: `Se insertaron ${insertedCount} participantes` });
+    }
   });
 });
 
-// Definir la ruta para crear asistentes relacionados con un registro
-app.post('/api/registros', (req, res) => {
-  const registro = req.body;
+// -------- SESIONES
+app.post('/sesiones', (req, res) => {
+  const {
+    cantParticipantes,
+    nombreAct,
+    nombreEspacio,
+    fecha,
+    notas,
+    materialNecesario,
+    internet,
+    mobiliarioAdecuado,
+    luzElectrica,
+    banos
+  } = req.body;
 
-  const sql = 'INSERT INTO registros SET ?';
-  db.query(sql, registro, (err, result) => {
+  const query = `INSERT INTO sesiones (cantParticipantes, nombreAct, nombreEspacio, fecha, notas, materialNecesario, internet, mobiliarioAdecuado, luzElectrica, banos) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const values = [
+    cantParticipantes,
+    nombreAct,
+    nombreEspacio,
+    fecha,
+    notas,
+    materialNecesario,
+    internet,
+    mobiliarioAdecuado,
+    luzElectrica,
+    banos
+  ];
+
+  db.query(query, values, (err, result) => {
     if (err) {
       console.error('Error al guardar el registro:', err);
       res.status(500).send('Error al guardar el registro');
-    } else {
-      console.log('Registro guardado exitosamente');
-      const registroId = result.insertId;
-      res.status(200).json({ id: registroId });
+      return;
     }
+
+    const registroId = result.insertId;
+    res.status(201).json({ message: 'Registro creado, id: ' + registroId });
   });
 });
 
-app.get('/api/registros/:registroId', (req, res) => {
-  const registroId = req.params.registroId;
-
-  const sql = 'SELECT * FROM registros WHERE id = ?';
-  db.query(sql, registroId, (err, result) => {
-    if (err) {
-      console.error('Error al obtener el registro:', err);
-      res.status(500).send('Error al obtener el registro');
-    } else {
-      const registro = result[0];
-      res.status(200).json(registro);
-    }
-  });
-});
-
-app.get('/api/comportamientos', (req, res) => {
-  const sql = 'SELECT * FROM comportamientos';
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(result.map((row) => row.comportamiento));
-  });
-});
-
-app.get('/api/aperturas', (req, res) => {
-  const sql = 'SELECT * FROM aperturas';
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(result.map((row) => row.apertura));
-  });
-});
-
-app.get('/api/registros/:registroId/asistentes', (req, res) => {
-  const registroId = req.params.registroId;
-
-  const sql = 'SELECT * FROM asistentes WHERE registro_id = ?';
-  db.query(sql, registroId, (err, result) => {
-    if (err) {
-      console.error('Error al obtener los asistentes:', err);
-      res.status(500).send('Error al obtener los asistentes');
-    } else {
-      res.status(200).json(result);
-    }
-  });
-});
-
-app.post('/api/registros/:registroId/asistentes', (req, res) => {
-  const registroId = req.params.registroId;
-  const asistentes = req.body;
-
-  // Mapear los asistentes para agregar el registroId
-  const asistentesConRegistroId = asistentes.map((asistente) => ({
-    ...asistente,
-    registro_id: registroId,
-  }));
-
-  const sql = 'INSERT INTO asistentes SET ?';
-  db.query(sql, asistentesConRegistroId, (err, result) => {
-    if (err) {
-      console.error('Error al registrar los asistentes:', err);
-      res.status(500).send('Error al registrar los asistentes');
-    } else {
-      console.log('Asistentes registrados exitosamente');
-      res.status(200).send(result);
-    }
-  });
-});
 
 // Iniciar el servidor en el puerto 3000
 app.listen(3000, () => {
